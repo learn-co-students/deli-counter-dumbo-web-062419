22e0f69929033cccdfe74b550ee549cc93db98a7
Olga Rosas <olga.rosas@gmail.com>
2019-06-03 14:11:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 katz_deli = []
 
 def take_a_number(katz_deli, new_person)
-  puts "Welcome #{new_person}. You are number " + katz_deli.length +  " in line."
+  katz_deli.push(new_person)
+  position = katz_deli.index(new_person)
+  puts "Welcome #{new_person}. You are number #{katz_deli.index(new_person)+1} in line."
+  return new_person, position
 end
 
 def line(katz_deli)
