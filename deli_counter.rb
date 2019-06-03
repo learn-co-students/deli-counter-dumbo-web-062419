3529d33a84684cc92d38fc82b23862aa8e8656ab
Olga Rosas <olga.rosas@gmail.com>
2019-06-03 13:44:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@ def line(katz_deli)
   if (katz_deli.length == 0)
     puts "The line is currently empty."
   else
-    katz_deli.push(new_person)
     puts "The line is currently: " + [i + 1] + ". " + katz_deli.shift + "."
 end
 end
